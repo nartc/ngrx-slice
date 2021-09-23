8613dad673262597de468b94237c55ab087c909d
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2021-09-22 21:15:49 -0500

    chore: update packages

--- projects/ngrx-slice/entity/karma.conf.js
+++ projects/ngrx-slice/entity/karma.conf.js
@@ -25,7 +25,10 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "../../coverage/ngrx-slice"),
+      dir: require("path").join(
+        __dirname,
+        "../../../coverage/ngrx-slice/entity"
+      ),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
